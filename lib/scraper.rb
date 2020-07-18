6a5c0ef7089906c4e3c6cbc48ee5c29d9c11b2c7
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-18 02:51:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,7 @@ class Scraper
     student = {}
     profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
+    
     links.each do |link|
       if link.include?("linkedin")
         student[:linkedin] = link
